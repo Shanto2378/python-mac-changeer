d843f4a2c8f492b85ba9b66f6f42fcdb380701cf
Shanto2378 <hasibul.shanto1@northsouth.edu>
2024-03-02 03:49:44 +0600

    just testing on linux vs code

--- mac changer.py
+++ mac changer.py
@@ -12,3 +12,5 @@ print(" [+] Changing MAC address for " + interface + " to " + new_mac)
 subprocess.call("ifconfig " + interface + " down", shell=True)
 subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
 subprocess.call("ifconfig " + interface + " up", shell=True)
+
+print("vs linux test")
