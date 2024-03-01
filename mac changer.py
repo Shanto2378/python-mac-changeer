5c7dfb567830917d3726364b01a32c86304f3d7f
Shanto2378 <hasibul.shanto1@northsouth.edu>
2024-03-02 02:11:48 +0600

    small edit beautifying the code using funcitons and variables

--- mac changer.py
+++ mac changer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+# This is a simple script to turn off the network interface for wlan0 and eth0
+
 import subprocess
 
-# This is a simple script to turn off the network interface for wlan0 and eth0
-# subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+interface = "wlan0"
+new_mac = "00:11:22:33:44:55"
+
+print(" [+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
