575b7ddb37700a85b0c9b3c5a1201f469e47176e
Shanto2378 <hasibul.shanto1@northsouth.edu>
2024-03-03 14:55:20 +0600

    taking user input for mac address

--- mac changer.py
+++ mac changer.py
@@ -12,5 +12,3 @@ print(" [+] Changing MAC address for " + interface + " to " + new_mac)
 subprocess.call("ifconfig " + interface + " down", shell=True)
 subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
 subprocess.call("ifconfig " + interface + " up", shell=True)
-
-print("vs linux test")
