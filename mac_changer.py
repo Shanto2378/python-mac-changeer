42c883b7a3a17406cfdf8d84541f9456ea50f813
Shanto2378 <hasibul.shanto1@northsouth.edu>
2024-05-01 04:25:31 +0600

    fixing some bugs

--- mac_changer.py
+++ mac_changer.py
@@ -1,51 +1,50 @@
-    #!/usr/bin/env python3
+#!/usr/bin/env python3
 
 # This is a simple script to turn off the network interface for wlan0 and eth0
 
-import subprocess # importing the subprocess module to run the system commands
-import optparse # importing the optparse module to parse the command line arguments
-import re # importing the re module to use regular expressions
+import subprocess
+import optparse
+import re
 
-# setting up function to change the MAC address
-def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change is's MAC address") # adding options to the parser for interface
-    parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address") # adding options to the parser for MAC address
-    (options, arguments) = parser.parse_args() # storing value in options and arguments
+def user_input():
+    parser = optparse.OptionParser() # create an object of OptionParser
+    parser.add_option("-i", "--interface", dest = "interface", help = "Enter a valid interface by -i or --interface") # add options to the parser
+    parser.add_option("-m", "--mac", dest = "new_mac", help = "Enter a valid mac_address by -m or --mac") # add options to the parser
+    (options, arguments) = parser.parse_args() # parse the options and arguments
+    if not options.interface: # if the user does not enter the interface
+        parser.error("Enter a valid interface use --help for more info") # print the error message
+    elif not options.new_mac: # if the user does not enter the mac address
+        parser.error("Enter a valid mac address use --help for more info") # print the error message
+    return options # return the options
+    
+def change_mac(interface, new_mac): # function to change the mac address
+    print("[+] Changing mac for " + interface + " to " + new_mac) # print the message
 
-    if not options.interface:
-        # code to handle error
-        parser.error(" [-] Please specify an interface, use --help for more information")
-    elif not options.new_mac:
-        # code to handle erro
-        parser.error(" [-] Please specify a new MAC address, use --help for more information")    
-    return options # returning the value of options to the function    
+    subprocess.call(["ifconfig", interface, "down"]) # turn off the interface
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac]) # change the mac address
+    subprocess.call(["ifconfig", interface, "up"]) # turn on the interface
 
-def change_mac(interface, new_mac):
-    print(" [+] Changing MAC address for " + interface + " to " + new_mac)
-    subprocess.call(["ifconfig", interface, "down"]) # turns of the interface to change the mac 
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac]) # changes the MAC address to given value
-    subprocess.call(["ifconfig", interface, "up"]) # turns on the interface after changing the MAC address
+def get_current_mac(interface): # function to get the current mac address
+    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode("UTF-8") # get the output of the ifconfig command
+    mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result) # search for the mac address in the output
 
-def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode("UTF-8") # storing the result of the ifconfig command in ifconfig_result in UTF-8 format
-    # read more at: https://discuss.python.org/t/typeerror-cannot-use-a-string-pattern-on-a-bytes-like-object/30308/2
-    mac_addr_src_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result) # using regular expression to search for the MAC address in the ifconfig_result
+    if mac_result: # if the mac address is found
+        return mac_result.group(0) # return the mac address
+    else: # if the mac address is not found
+        print("Could not read Mac Address") # print the message
 
-    if mac_addr_src_result:
-        return mac_addr_src_result.group(0) # returning the MAC address if found to current_mac
-    else:
-        print(" [-] Could not read MAC address.")
+options = user_input() # get the user input
 
-options = get_arguments() # getting the value of options and arguments from the function to work with change_mac function.
+current_mac = get_current_mac(options.interface) # get the current mac address
 
-current_mac = get_current_mac(options.interface) # calling the function to get the current MAC address
+print("[+] Current Interface", options.interface) # print the current interface
+print("[+] Current mac", current_mac) # print the current mac address
 
-print(" [+] Current MAC = ", current_mac) # printing the current MAC address
-change_mac(options.interface, options.new_mac) # calling the function to change the MAC address
+change_mac(options.interface, options.new_mac) # change the mac address
 
-current_mac = get_current_mac(options.interface)
-if current_mac == options.new_mac:
-    print(" [+] MAC address was successfully changed to ", current_mac) # printing the new MAC address
-else:
-    print(" [-] MAC address did not get changed.") # printing the message if the MAC address did not get changed
+current_mac = get_current_mac(options.interface) # get the current mac address
+
+if current_mac == options.new_mac: # if the mac address is changed
+    print("[+] Mac Address is changed to ", current_mac) # print the message
+else: # if the mac address is not changed
+    print("Mac Adress did not get changed") # print the message
